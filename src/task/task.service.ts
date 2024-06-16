8a7786d5077f29eebe965125022c3254d0516f15
Caique L. Fernandes <caique.l.f9.clf@gmail.com>
2024-06-15 21:36:11 -0400

    Criando endpoints

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@nestjs/common';
-import { TaskDto } from './task.dto';
+import { Get, HttpException, HttpStatus, Injectable, NotFoundException, Query } from '@nestjs/common';
+import { FindAllParameters, TaskDto } from './task.dto';
 
 @Injectable()
 export class TaskService {
@@ -8,6 +8,54 @@ export class TaskService {
 
     create(task : TaskDto){
         this.tasks.push(task);
-        console.log(this.tasks)
+    }
+
+    findById(id : string): TaskDto{
+        const foundTask = this.tasks.filter(t => t.id === id);
+
+        if(foundTask.length){
+            return foundTask[0]
+        }
+
+        // throw new NotFoundException(`Task with id ${id} not found`);
+        throw new HttpException(`Task with id ${id} not found`, HttpStatus.NOT_FOUND);
+    }
+
+    findAll(params: FindAllParameters): TaskDto[] {
+        return this.tasks.filter(t=>{
+            let match = true
+
+            if(params.title !== undefined && !t.title.includes(params.title)){
+                match = false;
+            }
+
+            if (params.status !== undefined && !t.status.includes(params.status)) {
+                match = false;
+            }
+
+            return match
+        })
+    }
+
+    update(task : TaskDto){
+        let taskIndex =this.tasks.findIndex(t => t.id ===task.id)
+
+        if(taskIndex >= 0){
+            this.tasks[taskIndex] = task;
+            return
+        }
+
+        throw new HttpException(`Task with id ${task.id} not found`, HttpStatus.BAD_REQUEST);
+    }
+
+    remove(id: string){
+        let taskIndex = this.tasks.findIndex(t => t.id === id);
+
+        if(taskIndex >= 0){
+            this.tasks.splice(taskIndex, 1);
+            return
+        }
+
+        throw new HttpException(`Task with id ${id} not found`, HttpStatus.BAD_REQUEST)
     }
 }
